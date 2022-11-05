324ae71e2e183c2020539dfa178fa1f903596e7b
yoni13 <yoni980807@gmail.com>
2022-11-05 13:18:39 +0800

    MY CTF

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask,render_template,request,abort,send_file
+app = Flask('__name__')
+
+import os
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/teas')
+def no():
+	return abort(403)
+@app.route('/getimg')
+def getimg():
+		try:
+			find = request.args.get('data')
+			print(os.getcwd()+'/statics/'+find)
+			return send_file(os.getcwd()+'/statics/' + find)
+		except:
+			return abort(404)
+app.run(host='0.0.0.0')
